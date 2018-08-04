4e3380f72703a36845e10b8f4ee9356786fc9cab
Manan <manansoni0@gmail.com>
2018-08-05 02:23:14 +0500

    Fix menu json files search path
    * menu json files path was missing a '/' at the end
    
    Tested-by: MananSoni42

--- src/query_sort.py
+++ src/query_sort.py
@@ -166,7 +166,7 @@ def get_meal(mealtype):
 
     if now != MESS['date']:
         try:
-            with open('db/menu' + now + ".json") as json_data:
+            with open('db/menu/' + now + ".json") as json_data:
                 today = json.load(json_data)
         except:
             return "<Sorry, Menu will be updated by tonight>"
